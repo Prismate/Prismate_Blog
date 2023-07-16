84eaf4eec96df9bc2d6727705262f8ab2950a571
prismate <mrpivotus@gmail.com>
2023-07-16 10:27:36 +0200

    flash massages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -30,9 +30,11 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash("Form Submitted Successfully!")
     return render_template('name.html',
         name = name,
         form = form)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
